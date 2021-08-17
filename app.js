a1427da2862678b41fcce667225737046012e200
Dnyaneshwar Mogal <maulimogal@gmail.com>
2021-08-17 23:25:19 +0530

    project revamp

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-"use strict";
+let initialPrice = document.querySelector("#initial-price");
+let stockQuantity = document.querySelector("#stock-quantity");
+let currentPrice = document.querySelector("#current-price");
+let submitBtn = document.querySelector("#submit-btn");
+let outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  let ip = initialPrice.value;
+  let qty = stockQuantity.value;
+  let curr = currentPrice.value;
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial < current) {
+    let profit = ((current - initial) * quantity).toFixed(2);
+    let profitPercentage = ((current - initial)/(initial/100)).toFixed(2);
+    outputBox.innerHTML =
+      "You gained " + profitPercentage + "%. Your total profit is ₹ " + profit;
+  } else if (initial > current) {
+    let loss = ((initial - current) * quantity).toFixed(2);
+    let lossPercentage = ((initial - current)/(initial/100)).toFixed(2);
+    outputBox.innerHTML =
+      "You lost " + lossPercentage + "%. Your total loss is ₹ " + loss;
+  } else {
+    outputBox.innerHTML = "Even Steven";
+  }
+}
