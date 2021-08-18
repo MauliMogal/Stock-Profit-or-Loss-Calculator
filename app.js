ca9ed56031866e38f7b91a358643ec0ad42f11d5
Dnyaneshwar Mogal <maulimogal@gmail.com>
2021-08-18 09:36:18 +0530

    footer

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ function calculateProfitAndLoss(initial, quantity, current) {
     let profit = ((current - initial) * quantity).toFixed(2);
     let profitPercentage = ((current - initial) / (initial / 100)).toFixed(2);
     outputBox.innerHTML =
-      "You gained " + profitPercentage + "%. Your total profit is ₹ " + profit;
+      "You gained " + profitPercentage + "%. </br>Your total profit is ₹ " + profit;
   } else if (initial > current) {
     let loss = ((initial - current) * quantity).toFixed(2);
     let lossPercentage = ((initial - current) / (initial / 100)).toFixed(2);
     outputBox.innerHTML =
-      "You lost " + lossPercentage + "%. Your total loss is ₹ " + loss;
+      "You lost " + lossPercentage + "%. </br>Your total loss is ₹ " + loss;
   } else {
     outputBox.innerHTML = "Even Steven";
   }
